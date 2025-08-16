e8087537ce3db546ea41767254007b592c2c110e
Zlogger <79506370+haker20SZs@users.noreply.github.com>
2025-08-16 12:23:07 +0700

    Update general.bat

--- general.bat
+++ general.bat
@@ -12,6 +12,10 @@ set "DIR=%~dp0"
 
 --filter-udp=443 --ipset='%DIR%\ipset\ipset-cloudflare.txt' --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic='%DIR%\app\quic_initial_www_google_com.bin' --new ^
 
+--filter-tcp=443,1024-65535 --ipset='%DIR%\ipset\ipset-all.txt' --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=md5sig,badseq --new ^
+
+--filter-udp=1024-65535 --ipset='%DIR%\ipset\ipset-all.txt' --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp='%DIR%\app\quic_initial_www_google_com.bin' --dpi-desync-cutoff=n2 --new ^
+
 --filter-tcp=80 --ipset='%DIR%\ipset\ipset-cloudflare.txt' --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --ipset='%DIR%\ipset\ipset-cloudflare.txt' --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig
 
 exit
